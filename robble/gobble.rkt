5f1ec1c9f8da1121ca799dea6330a2b6bedbb6c3
jitwit <jrn@bluefarm.ca>
2020-05-13 15:52:19 -0400

    save

--- robble/gobble.rkt
+++ robble/gobble.rkt
@@ -1,26 +1,29 @@
-#lang racket
+#lang racket/base
 
 (require web-server/servlet
          web-server/servlet-env
-         net/rfc6455)
+         web-server/dispatch
+         net/rfc6455
+         net/url)
 
 ;;;; SETTINGS
-; defaults are development ports? idk...
-(define gobble-port
+(define gobble-websocket-port
+  (make-parameter 9009))
+(define gobble-http-port
   (make-parameter 9009))
 (define gobble-dir
   (make-parameter "/Users/jrn/code/gobble/garcon"))
 
 ;;;; HOME
 (define (gobbler/http req)
+  (display req) (newline)
   (response/xexpr
    `(html
      (head
       (title "gobble")
-      (link ((rel "stylesheet") (href "static/gobble.css")))
-      ;; (script ((src "static/jquery-3.4.1.slim.js")))
-      ;; (script ((src "static/gobble.js")))
-      )
+      (link ((rel "stylesheet") (href "gobble.css")))
+      (script ((src "jquery-3.4.1.slim.js")))
+      (script ((src "robble.js"))))
      (body
       (h1 "GOBBLE")
       (p ,(string-append "I'm alive: " (number->string (current-seconds))))
@@ -52,29 +55,31 @@
                      (div ((id "solution"))))
                 (div ((id "scores")))))))))
 
-
 ;;;; GAME SERVER
 (define (websocket-server)
-  (ws-serve #:port (sub1 (gobble-port)) (lambda (c s) (ws-send! c "hiho"))))
+  (ws-serve #:port (gobble-websocket-port)
+            (lambda (c s)
+              (display `(hiho ,c ,s))
+              (newline)
+              
+              )))
 
 (define (http-server)
   (serve/servlet gobbler/http
-                 #:port (gobble-port)
-                 #:extra-files-paths `(,(gobble-dir))
-                 #:servlet-path "/"
-                 #:servlet-current-directory (gobble-dir)
+                 #:port (gobble-http-port)
+                 #:extra-files-paths `(,(string-append (gobble-dir) "/static"))
+                 #:listen-ip #f
+                 #:servlet-path "/boggle-bitch"
                  #:command-line? #t))
 
-(define thread/wss
-  (thread websocket-server))
-(define thread/http
-  (thread http-server))
-
 (define (main)
+  (display 'hiho) (newline)
+  (define thread/wss
+    (thread websocket-server))
+  (define thread/http
+    (thread http-server))
   (let loop ()
-    (display (current-inexact-milliseconds)) (newline)
-    (sleep 2)
+    (sleep 0.1)
     (loop)))
 
 (main)
-
