80041b2fb4e59516dcdc896b80d2eca83a55db40
jitwit <jrn@bluefarm.ca>
2020-05-14 13:26:40 -0400

    systemd timer for boards

--- deploy/app.nix
+++ deploy/app.nix
@@ -22,25 +22,28 @@ in
       euler
       cobble ];
   
-  systemd.services.cobble =
-    { description = "make board for gobble";
-      wantedBy = [ "multi-user.target" ];
-      environment = { CHEZSCHEMELIBDIRS = "${libdirs}"; };
-      serviceConfig =
-        { WorkingDirectory = "${cobble}";
-          ExecStart = "${cobble}/bin/gobbler -n 1000 -d /var/www/gobble/boards";
-        };
-    };
+  systemd = {
+    timers.cobble = {
+      wantedBy = [ "timers.target" ];
+      partOf = [ "cobble.service" ];
+      timerConfig.OnCalendar = "daily"; };
 
-  systemd.services.gobble =
-    { description = "boggle-bitch.net";
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" "cobble.service" ];
-      wants = [ "cobble.service" ];
-      serviceConfig =
-        { WorkingDirectory = "/var/www/gobble";
-          ExecStart = "${gobbler}/bin/gobble 80";
-        };
+    services.cobble =
+      { description = "make board for gobble";
+        wantedBy = [ "multi-user.target" ];
+        environment = { CHEZSCHEMELIBDIRS = "${libdirs}"; };
+        serviceConfig =
+          { WorkingDirectory = "${cobble}";
+            ExecStart = "${cobble}/bin/gobbler -n 1000 -d /var/www/gobble/boards"; }; };
+
+    services.gobble =
+      { description = "boggle-bitch.net";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network.target" "cobble.service" ];
+        wants = [ "cobble.service" ];
+        serviceConfig =
+          { WorkingDirectory = "/var/www/gobble";
+            ExecStart = "${gobbler}/bin/gobble 80"; }; };
     };
   };
 }
