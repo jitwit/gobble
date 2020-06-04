20a87fd94d886e0871891877bcc71607954606e3
jitwit <jrn@bluefarm.ca>
2020-06-04 07:18:02 -0400

    improve boards, probably

--- nibble/chez-hemlock.nix
+++ nibble/chez-hemlock.nix
@@ -6,22 +6,18 @@ stdenv.mkDerivation {
 
   src = fetchgit {
     url = "https://github.com/jitwit/hemlock.git";
-    rev = "ef32f8e181dcd45d6b4dc183f9724304ed4a9ac2";
-    sha256 = "1rq5x1bcslxhgwfjz0rgws4j302yl5qkbmbwd08znpcxyl41vaya";
+    rev = "d26beffa520c7ae91c1f4d9c1aaa0b0708a2a41b";
+    sha256 = "0mylkcvi3cdyw95vhn8is4df0p6a31wyc4v1g8a0lv8lwzpi042y";
   };
 
   buildInputs = [ chez ];
 
-  buildPhase = ''
-    make prefix=$out CHEZ=${chez}/bin/scheme
-  '';
+  buildPhase = "make prefix=$out CHEZ=${chez}/bin/scheme";
 
-    installPhase = ''
-    make install prefix=$out CHEZ=${chez}/bin/scheme
-  '';
+  installPhase = "mkdir -p $out/lib/csv9.5-site && cp -r *.so $out/lib/csv9.5-site";
 
   meta = {
-    description = "This package contains implementations of tree structures and others";
+    description = "Trees and other structures";
     homepage = https://github.com/jitwit/chez-hemlock/;
     maintainers = [ stdenv.lib.maintainers.jitwit ];
     license = stdenv.lib.licenses.gpl3;
